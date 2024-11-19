8bc07a3fe8eb1476212ea0a9ac0bff180f30b708
FreseFea <fresefae@gmail.com>
2024-11-19 18:29:28 +0300

    1 zadanie completed

---
+++ 1zadan/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var schetchik int
+var mutex sync.Mutex
+
+func incr() {
+	mutex.Lock()
+	schetchik++
+	mutex.Unlock()
+}
+
+func main() {
+	for i := 0; i < 5; i++ {
+		go incr()
+	}
+
+	time.Sleep(time.Second)
+
+	fmt.Println("Итоговое значение счетчика:", schetchik)
+}
