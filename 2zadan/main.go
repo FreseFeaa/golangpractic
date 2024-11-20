176b675577f7ec9ae5589be0b2b7e6f09057a6ba
FreseFea <fresefae@gmail.com>
2024-11-20 11:44:16 +0300

    2 zadanie update

--- 2zadan/main.go
+++ 2zadan/main.go
@@ -1,30 +1,46 @@
+// ## Задание 2
+// Записать в канал 10 числовых значений в потоке основной горутины.
+// Запустить чтение из канала внутри других 10 горутин.
+// Каждая горутина должна преобразовывать числовое значение в строку и складывать во второй канал.
+// Основная горутина должна читать из второго канала строки и выводить их на экран
+// в порядке поступления из канала.
+// Строчка с запуском горутины в коде должна быть одна (go ...)
+// В результате в консоль должно распечататься 10 чисел в строковом представлении.
+
 package main
 
 import (
 	"fmt"
 	"strconv"
+	"sync"
 )
 
 func main() {
-	numChannel := make(chan int)
-	strChannel := make(chan string)
+	channlNum := make(chan int, 10)
+	channlStr := make(chan string, 10)
 
-	go func() {
-		for i := 0; i < 10; i++ {
-			numChannel <- i
-		}
-		close(numChannel)
-	}()
+	var waitgrp sync.WaitGroup
+
+	for i := 1; i <= 10; i++ {
+		channlNum <- i
+	}
+	close(channlNum)
 
 	for i := 0; i < 10; i++ {
+		waitgrp.Add(1)
 		go func() {
-			num := <-numChannel
-			strChannel <- strconv.Itoa(num)
+			defer waitgrp.Done()
+			num := <-channlNum
+			str := strconv.Itoa(num)
+			channlStr <- str
 		}()
 	}
 
+	waitgrp.Wait()
+	close(channlStr)
+
 	fmt.Println("Итоговый канал строк: ")
-	for i := 0; i < 10; i++ {
-		fmt.Println("Число в строке:", <-strChannel)
+	for stroka := range channlStr {
+		fmt.Println("Число в строке: ", stroka)
 	}
 }
